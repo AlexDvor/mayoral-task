4f1dc0e3d99530c77dd3eb875c9e187523d377d9
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-06-15 12:59:47 +0200

    add product list

--- src/components/Product-list/Product-list.styled.tsx
+++ src/components/Product-list/Product-list.styled.tsx
@@ -1,52 +1,87 @@
 import styled from '@emotion/styled';
 
-export const Wrapper = styled.div``;
-
 export const List = styled.ul`
   display: flex;
   flex-wrap: wrap;
-  margin-top: -30px;
-  margin-left: -30px;
+  justify-content: center;
+  margin-top: -20px;
+  margin-left: -20px;
   padding: 0px;
-  margin: 0px;
 `;
 
 export const Item = styled.li`
-  /* flex-basis: calc(100% / 1 - 30px);
-  margin-top: 30px;
-  margin-left: 30px; */
+  margin-top: 20px;
+  margin-left: 20px;
   outline: 1px solid #0172ea;
-  width: 425px;
-  padding: 25px;
+  background-color: white;
+  padding: 10px;
+  flex-basis: calc(100% / 2 - 40px);
+  border-radius: 1%;
+  box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.12), 0px 4px 4px rgba(0, 0, 0, 0.06),
+    1px 4px 6px rgba(0, 0, 0, 0.16);
+
+  @media screen and (min-width: 320px) {
+  }
+
+  @media screen and (min-width: 768px) {
+    flex-basis: calc(100% / 3 - 60px);
+  }
+  @media screen and (min-width: 1024px) {
+    flex-basis: calc(100% / 4 - 80px);
+  }
 `;
 
-export const WrapperImg = styled.div``;
-
-export const Title = styled.h1`
-  font-size: 20px;
+export const WrapperImg = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
 `;
 
-export const Price = styled.p``;
-export const DiscountPrice = styled.p``;
 export const WrapperInfo = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
+
+export const Title = styled.h1`
+  text-align: center;
+  font-size: 14px;
+  margin-top: 15px;
+`;
+
+export const Price = styled.p`
+  font-size: 14px;
+  margin-top: 25px;
+`;
+
+export const DiscountPrice = styled.p`
+  font-size: 14px;
+  margin-top: 15px;
+`;
+
+export const ColorButton = styled.button`
+  /* padding: 10px; */
+  font-size: 14px;
+  margin-top: 15px;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+`;
+
 export const AddButton = styled.button`
   font-size: 14px;
-  height: 46px;
+  height: 35px;
   width: 100px;
   border: none;
   border-radius: 3px;
   background-color: #0172ea;
   color: #fff;
   text-transform: uppercase;
+  margin-top: 15px;
   cursor: pointer;
-`;
 
-export const ColorButton = styled.button`
-  background-color: transparent;
-  border: none;
-  cursor: pointer;
+  &:hover,
+  &:focus {
+    background-color: #004997;
+  }
 `;
--- src/components/Product-list/Product-list.tsx
+++ src/components/Product-list/Product-list.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import {
-  Wrapper,
   List,
   Item,
   WrapperImg,
@@ -16,30 +15,113 @@ import im from '../../../public/images/im.jpg';
 const ProductList = () => {
   return (
     <>
-      <Wrapper>
-        <List>
-          <Item>
-            <WrapperImg>
-              <Image
-                src={im}
-                alt="Picture of the author"
-                // width={500}
-                // height={500}
-
-                // blurDataURL="data:..." automatically provided
-                // placeholder="blur" // Optional blur-up while loading
-              />
-            </WrapperImg>
-            <WrapperInfo>
-              <Title>Polo manga corta ECOFRIENDS</Title>
-              <Price>25.30</Price>
-              <DiscountPrice>10.99</DiscountPrice>
-              <ColorButton type="button"> + Mas colores</ColorButton>
-              <AddButton type="button">Añadir</AddButton>
-            </WrapperInfo>
-          </Item>
-        </List>
-      </Wrapper>
+      <List>
+        {/*  */}
+        <Item>
+          <WrapperImg>
+            <Image src={im} alt="Picture of the author" />
+          </WrapperImg>
+          <WrapperInfo>
+            <Title>Polo manga corta ECOFRIENDS</Title>
+            <Price>25.30</Price>
+            <DiscountPrice>10.99</DiscountPrice>
+            <ColorButton type="button">mas colores</ColorButton>
+            <AddButton type="button">Añadir</AddButton>
+          </WrapperInfo>
+        </Item>
+        {/*  */}
+        <Item>
+          <WrapperImg>
+            <Image src={im} alt="Picture of the author" />
+          </WrapperImg>
+          <WrapperInfo>
+            <Title>Polo manga corta ECOFRIENDS</Title>
+            <Price>25.30</Price>
+            <DiscountPrice>10.99</DiscountPrice>
+            <ColorButton type="button">mas colores</ColorButton>
+            <AddButton type="button">Añadir</AddButton>
+          </WrapperInfo>
+        </Item>
+        {/*  */}
+        <Item>
+          <WrapperImg>
+            <Image src={im} alt="Picture of the author" />
+          </WrapperImg>
+          <WrapperInfo>
+            <Title>Polo manga corta ECOFRIENDS</Title>
+            <Price>25.30</Price>
+            <DiscountPrice>10.99</DiscountPrice>
+            <ColorButton type="button">mas colores</ColorButton>
+            <AddButton type="button">Añadir</AddButton>
+          </WrapperInfo>
+        </Item>
+        {/*  */}
+        <Item>
+          <WrapperImg>
+            <Image src={im} alt="Picture of the author" />
+          </WrapperImg>
+          <WrapperInfo>
+            <Title>Polo manga corta ECOFRIENDS</Title>
+            <Price>25.30</Price>
+            <DiscountPrice>10.99</DiscountPrice>
+            <ColorButton type="button">mas colores</ColorButton>
+            <AddButton type="button">Añadir</AddButton>
+          </WrapperInfo>
+        </Item>
+        {/*  */}
+        <Item>
+          <WrapperImg>
+            <Image src={im} alt="Picture of the author" />
+          </WrapperImg>
+          <WrapperInfo>
+            <Title>Polo manga corta ECOFRIENDS</Title>
+            <Price>25.30</Price>
+            <DiscountPrice>10.99</DiscountPrice>
+            <ColorButton type="button">mas colores</ColorButton>
+            <AddButton type="button">Añadir</AddButton>
+          </WrapperInfo>
+        </Item>
+        {/*  */}
+        <Item>
+          <WrapperImg>
+            <Image src={im} alt="Picture of the author" />
+          </WrapperImg>
+          <WrapperInfo>
+            <Title>Polo manga corta ECOFRIENDS</Title>
+            <Price>25.30</Price>
+            <DiscountPrice>10.99</DiscountPrice>
+            <ColorButton type="button">mas colores</ColorButton>
+            <AddButton type="button">Añadir</AddButton>
+          </WrapperInfo>
+        </Item>
+        {/*  */}
+        <Item>
+          <WrapperImg>
+            <Image src={im} alt="Picture of the author" />
+          </WrapperImg>
+          <WrapperInfo>
+            <Title>Polo manga corta ECOFRIENDS</Title>
+            <Price>25.30</Price>
+            <DiscountPrice>10.99</DiscountPrice>
+            <ColorButton type="button">mas colores</ColorButton>
+            <AddButton type="button">Añadir</AddButton>
+          </WrapperInfo>
+        </Item>
+        {/*  */}
+        <Item>
+          <WrapperImg>
+            <Image src={im} alt="Picture of the author" />
+          </WrapperImg>
+          <WrapperInfo>
+            <Title>Polo manga corta ECOFRIENDS</Title>
+            <Price>25.30</Price>
+            <DiscountPrice>10.99</DiscountPrice>
+            <ColorButton type="button">mas colores</ColorButton>
+            <AddButton type="button">Añadir</AddButton>
+          </WrapperInfo>
+        </Item>
+        {/*  */}
+      </List>
     </>
   );
 };
